65a8f5596307099dba8482691900e72c9e2ac88a
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2025-10-06 18:56:15 +0300

    remove name attribute from models

--- ls/config/dataclasses/models.py
+++ ls/config/dataclasses/models.py
@@ -3,7 +3,7 @@ from typing import List, Optional, Dict, Literal, Any
 
 @dataclass
 class CNN6Config:
-    name: str = "cnn6"
+    # name: str = "cnn6"
     do_dropout: bool = True
     label_dim: int = 4
     backbone_only: bool = False
@@ -12,14 +12,14 @@ class CNN6Config:
 
 @dataclass
 class ASTConfig:
-    name: str = "ast"
-    audioset_pretrained: bool = True
+    # name: str = "ast"
+    audioset_pretrain: bool = True
     input_fdim: int = 128
     input_tdim: int = 1024
     model_size: str = "base384"
     fstride: int = 10
     tstride: int = 10
-    imagenet_pretrained: bool = False
+    imagenet_pretrain: bool = False
     label_dim: int = 4
     backbone_only: bool = False
     cpt_path: Optional[str] = None
