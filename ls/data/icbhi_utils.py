198fbc33d3aebf075943cd9e786c00a0f4fd4b5d
gkontogiannhs <g.kontogiannis@ac.upatras.gr>
2025-12-15 14:33:19 +0200

    start point

--- ls/data/icbhi_utils.py
+++ ls/data/icbhi_utils.py
@@ -112,7 +112,7 @@ def _extract_lungsound_annotation(file_name: str, data_folder: str) -> Tuple[pd.
         delimiter="\t",
     )
     # Add metadata columns to annotation rows
-    recording_annotations["PID  "] = tokens[0]
+    recording_annotations["PID"] = tokens[0]
     recording_annotations["Site"] = tokens[2]               # Chest location (LU, LL, etc.)
     recording_annotations["Mode"] = tokens[3]               # Acquisition mode
     recording_annotations["Device"] = tokens[4]             # Recording equipment
